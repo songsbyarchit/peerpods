0b0e3d7b35bd5daa81ece780e65d1e1402e52ce9
Archit Sachdeva <archit.sachdeva007@gmail.com>
2025-04-16 13:30:10 +0100

    added the localhost and 127.0.0.1 as seperate origins which are explicitly and seperately allowed

--- backend/app.py
+++ backend/app.py
@@ -13,7 +13,7 @@ app.include_router(auth_routes.router, prefix="/auth")
 # Allow frontend to talk to backend during dev
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
